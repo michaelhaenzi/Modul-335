31c83f0a4f1c34af90d91567d4779e1badfd3e87
Michael Hensi <michael.haenzi@geoinfo.ch>
2016-11-29 08:59:53 +0100

    Change file structure, Server response and client

---
+++ src/api/public/index.php
@@ -0,0 +1,14 @@
+<?php
+use Slim\Http\Request as Request;
+use Slim\Http\Response as Response;
+
+require '../vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+$app->run();
