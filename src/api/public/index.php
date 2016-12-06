16e7b0791c5581c69c280e7f654dd4c296fb8f7e
Michael Hensi <michael.haenzi@geoinfo.ch>
2016-12-06 13:20:06 +0100

    get images for users

--- src/api/public/index.php
+++ src/api/public/index.php
@@ -65,7 +65,7 @@ $app->add(new \Slim\Middleware\JwtAuthentication([
     "rules" => [
         new \Slim\Middleware\JwtAuthentication\RequestPathRule([
             "path" => "/",
-            "passthrough" => ["/api/". VERSION ."/auth", "/api/". VERSION ."/register", "/api/" . VERSION . "/files/images"]
+            "passthrough" => ["/api/". VERSION ."/auth", "/api/". VERSION ."/register", "/api/" . VERSION . '/files/images/{data:\w+.\w+}']
         ]),
         new \Slim\Middleware\JwtAuthentication\RequestMethodRule([
             "passthrough" => ["OPTIONS"]
